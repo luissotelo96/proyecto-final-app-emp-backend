e3f85bf7dda237f6e1a8004288dc4155b171ca11
luissotelo96 <soteloxd@gmail.com>
2024-02-20 18:42:25 -0500

    Primer commit

---
+++ src/main/java/com/eam/proyectofinalappemp/ProyectoFinalAppEmpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eam.proyectofinalappemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalAppEmpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalAppEmpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eam/proyectofinalappemp/ProyectoFinalAppEmpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam.proyectofinalappemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalAppEmpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
